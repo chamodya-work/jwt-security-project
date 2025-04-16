26865286bc35c4a57be399432812b6c141daf7f4
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-16 21:45:10 +0530

    define methods for check spring security

--- src/main/java/com/security/jwt/controller/UserController.java
+++ src/main/java/com/security/jwt/controller/UserController.java
@@ -4,10 +4,7 @@ import com.security.jwt.entity.User;
 import com.security.jwt.service.UserService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -15,12 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+
     @PostMapping("/create-new-user")
     public User registerNewUser(@RequestBody User user) {
         return userService.registerNewUser(user);
     }
-    @PostConstruct
-    public void initRoleAndUser(){
+
+    @PostConstruct //this method automatically run ,when application start
+    public void initRoleAndUser() {
         userService.initRoleAndUser();
     }
+
+    @GetMapping("/for-admin")
+    public String forAdmin() {
+        return "this is accessible for admin only";
+    }
+
+    @GetMapping("/for-user")
+    public String forUser() {
+        return "this is accessible for user only";
+    }
+
 }
